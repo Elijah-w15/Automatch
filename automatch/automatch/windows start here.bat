274bf29fc71b6636cae3134df66de345d8f9a00a
Elijah-w15 <elijahwhiting21@gmail.com>
2026-06-15 20:43:29 -0600

    Delete automatch/automatch directory

--- automatch/automatch/windows start here.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem ====================================================================
-rem  automatch -- double-click this to set everything up and run.
-rem  The program files are in the project_files folder; you do not need
-rem  to open it. This just launches the setup in there.
-rem ====================================================================
-call "%~dp0project_files\GO.bat"
